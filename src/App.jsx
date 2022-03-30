6da0c21c8a82edba90f1a7daed0463e3c0d6b86d
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2022-03-30 17:28:39 +0200

    making register jsx and css component desgin

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Single from './components/component/pages/single/Single'
 import Write from './components/component/pages/write/Write'
 import Settings from './components/component/pages/settings/Settings'
 import Login from './components/component/pages/login/Login'
+import Register from './components/component/pages/register/Register'
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <Login />
+      <Register />
     </>
   )
 }
--- src/components/component/pages/register/Register.jsx
+++ src/components/component/pages/register/Register.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Register() {
   return (
     <div className="container container_register">
-        
+        <span className="register_title">Register</span>
+        <form className="register_form">
+        <label>Username</label>
+        <input className="register_input" type="text" placeholder="Enter your username..." />
+        <label>Email</label>
+        <input className="register_input" type="text" placeholder="Enter your email..." />
+        <label>Password</label>
+        <input className="register_input" type="password" placeholder="Enter your password..." />
+        <button className="register_button">Register</button>
+      </form>
+        <button className="register_login-button">Login</button>
     </div>
   )
 }
