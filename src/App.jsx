019ec5817d6159b90b9cedd89ed0ed4d617d2958
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2022-03-30 17:24:34 +0200

    making login jsx and css component desgin

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Home from './components/component/pages/home/Home'
 import Single from './components/component/pages/single/Single'
 import Write from './components/component/pages/write/Write'
 import Settings from './components/component/pages/settings/Settings'
+import Login from './components/component/pages/login/Login'
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Login />
     </>
   )
 }
--- src/components/component/pages/login/Login.jsx
+++ src/components/component/pages/login/Login.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Login() {
   return (
     <div className="container container_login">
-        
+        <span className="login_title">Login</span>
+      <form className="login_form">
+        <label>Email</label>
+        <input className="login_input" type="text" placeholder="Enter your email..." />
+        <label>Password</label>
+        <input className="login_input" type="password" placeholder="Enter your password..." />
+        <button className="login_button">Login</button>
+      </form>
+        <button className="login_register-button">Register</button>
     </div>
   )
 }
